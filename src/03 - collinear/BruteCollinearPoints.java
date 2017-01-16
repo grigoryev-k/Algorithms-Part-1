8bb3ed88ba4a21d6de701a644acb0aee54ff8f7b
grigoryev <grigoryev@TH-81.TECHNOHIM.local>
2017-01-16 16:39:01 +0300

    Template for BruteCollinearPoints

--- src/03 - collinear/BruteCollinearPoints.java
+++ src/03 - collinear/BruteCollinearPoints.java
@@ -1,4 +1,19 @@
 
 public class BruteCollinearPoints {
+	private Point[] ps;
+	
+	public BruteCollinearPoints(Point[] points) {
+		if (points == null) {
+			throw new NullPointerException();
+		}
+		ps = points;
+	}
 
+	public int numberOfSegnments() {
+		
+	}
+	
+	public LineSegment[] segments() {
+		
+	}
 }
