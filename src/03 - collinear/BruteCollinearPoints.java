9d538b3b6a49de08f234021083ab494e3dff9373
grigoryev <grigoryev@TH-81.TECHNOHIM.local>
2017-01-17 11:59:16 +0300

    Fixed typo

--- src/03 - collinear/BruteCollinearPoints.java
+++ src/03 - collinear/BruteCollinearPoints.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class BruteCollinearPoints {
 	private Point[] ps;
+	private List<LineSegment> segs;
 	
 	public BruteCollinearPoints(Point[] points) {
 		if (points == null) {
 			throw new NullPointerException();
 		}
 		ps = points;
+		segs = new ArrayList<>();
+		
 	}
 
-	public int numberOfSegnments() {
+	public int numberOfSegments() {
 		
 	}
 	
